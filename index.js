2c35745aa66bb8b8696815143cf893cd483f73c6
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2022-11-07 08:22:08 +0300

    created the calculator

---
+++ index.js
@@ -0,0 +1,24 @@
+function addition() {
+    let x = prompt('Введите первое слогаемое');
+    let y = prompt('Введите второе слогаемое');
+    let sum = +x + +y;
+    alert(sum);
+}
+function deduction() {
+    let minuend = prompt('Из скольки вычесть (введите число)?');
+    let subtrahend = prompt('Сколько вычесть (введите число)?');
+    let difference = minuend - subtrahend;
+    alert(difference);
+}
+function multiplication() {
+    let multiplier1 = prompt('Какое число умножить?');
+    let multiplier2 = prompt('На какое число умножить?');
+    let result = multiplier1 * multiplier2;
+    alert(result);
+}
+function division() {
+    let n = prompt('Какое число делим?');
+    let q = prompt('На какое число делим?');
+    let result = n/q;
+    alert(result);
+}
