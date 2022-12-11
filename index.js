ff4bf78206fd55d6f4aff030e77ad64138d927f6
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2022-12-11 05:37:50 +0300

    added inputs, innerHTML; removed prompts and alerts; styled the document

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 function addition() {
-    let x = prompt('Введите первое слогаемое');
-    let y = prompt('Введите второе слогаемое');
-    let sum = +x + +y;
-    alert(sum);
+    let x = document.getElementById('summand1');
+    let y = document.getElementById('summand2');
+    let sum = +x.value + +y.value;
+    let result = document.getElementById('additionResult').innerHTML = sum;
 }
 function deduction() {
-    let minuend = prompt('Из скольки вычесть (введите число)?');
-    let subtrahend = prompt('Сколько вычесть (введите число)?');
-    let difference = minuend - subtrahend;
-    alert(difference);
-}
-function multiplication() {
-    let multiplier1 = prompt('Какое число умножить?');
-    let multiplier2 = prompt('На какое число умножить?');
-    let result = multiplier1 * multiplier2;
-    alert(result);
+    let minuend = document.getElementById('minuend');
+    let subtrahend = document.getElementById('subtrahend');
+    let difference = minuend.value - subtrahend.value;
+    let result = document.getElementById('deductionResult').innerHTML = difference;
 }
 function division() {
-    let n = prompt('Какое число делим?');
-    let q = prompt('На какое число делим?');
-    if(q==0) {
-        alert('На ноль делить нелья!');
+    let dividend = document.getElementById('dividend');
+    let divisor = document.getElementById('divisor');
+    if(Number(divisor.value)===0) {
+        let error = 'На ноль делить нелья!';
+        document.getElementById('divisionResult').innerHTML = error;
     }
     else {
-        let result = n/q;
-        alert(result);
+        let quotient = dividend.value/divisor.value;
+        let result = document.getElementById('divisionResult').innerHTML = quotient;
     }
+}
+function multiplication() {
+    let multiplier1 = document.getElementById('multiplier1');
+    let multiplier2 = document.getElementById('multiplier2');
+    let product = multiplier1.value * multiplier2.value;
+    let result = document.getElementById('multiplicationResult').innerHTML = product;
 }
